18ae23e657a2705ed3e462042c2f0405678e9d5f
Lisa Wells <lisawells999@gmail.com>
2016-06-10 17:37:59 -0400

    replace source sans pro with muli

--- functions.php
+++ functions.php
@@ -38,7 +38,9 @@ define( 'CHILD_THEME_VERSION', '2.2.3' );
 add_action( 'wp_enqueue_scripts', 'genesis_sample_enqueue_scripts_styles' );
 function genesis_sample_enqueue_scripts_styles() {
 
-	wp_enqueue_style( 'genesis-sample-fonts', '//fonts.googleapis.com/css?family=Source+Sans+Pro:400,600,700', array(), CHILD_THEME_VERSION );
+	// wp_enqueue_style( 'genesis-sample-fonts', '//fonts.googleapis.com/css?family=Source+Sans+Pro:400,600,700', array(), CHILD_THEME_VERSION );
+	wp_enqueue_style( 'genesis-sample-fonts', '//fonts.googleapis.com/css?family=Muli:400,300', array(), CHILD_THEME_VERSION );
+
 	wp_enqueue_style( 'dashicons' );
 
 	wp_enqueue_script( 'genesis-sample-responsive-menu', get_stylesheet_directory_uri() . '/js/responsive-menu.js', array( 'jquery' ), '1.0.0', true );
