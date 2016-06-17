b5ca50be1f70e55ef05548e5eb3faf5ea8db0992
Lisa Wells <lisawells999@gmail.com>
2016-06-17 18:15:36 -0400

    unenqueue satisfy font

--- functions.php
+++ functions.php
@@ -39,7 +39,7 @@ add_action( 'wp_enqueue_scripts', 'genesis_sample_enqueue_scripts_styles' );
 function genesis_sample_enqueue_scripts_styles() {
 
 	// wp_enqueue_style( 'genesis-sample-fonts', '//fonts.googleapis.com/css?family=Source+Sans+Pro:400,600,700', array(), CHILD_THEME_VERSION );
-	wp_enqueue_style( 'genesis-sample-fonts', '//fonts.googleapis.com/css?family=Satisfy|Muli:400,300', array(), CHILD_THEME_VERSION );
+	wp_enqueue_style( 'genesis-sample-fonts', '//fonts.googleapis.com/css?family=Muli:400,300', array(), CHILD_THEME_VERSION );
 
 	wp_enqueue_style( 'dashicons' );
 
