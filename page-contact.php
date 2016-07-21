ddec259326ad8c86c299cab00d7ecf205db6592b
Lisa Wells <lisawells999@gmail.com>
2016-07-21 11:30:27 -0400

    pulled from SG; add contact page template

---
+++ page-contact.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Genesis Sample.
+ */
+
+//* Template Name: Contact
+
+
+add_action('genesis_after_content_sidebar_wrap', 'bh_contact_add_imageband');
+function bh_contact_add_imageband() { ?>
+    <div class="contact-imageband">
+        <img class="alignnone size-full wp-image-287 thirds" src="http://staging3.buckhillbrewery.com/wp-content/uploads/2016/06/horse-parking.jpg" alt="Horse Parking" width="500" height="324" />
+        <img class="alignnone size-full wp-image-288 thirds" src="http://staging3.buckhillbrewery.com/wp-content/uploads/2016/06/Buck-Hill-Brewery-Drections.jpg" alt="Buck-Hill-Brewery-Drections" width="500" height="324" />
+        <img class="alignnone size-full wp-image-289 thirds" src="http://staging3.buckhillbrewery.com/wp-content/uploads/2016/06/restaurant-motorcycle-parking.jpg" alt="Motorcycle Parking" width="500" height="324" />
+    </div>
+<?php }
+
+//* Run the Genesis loop
+genesis();
