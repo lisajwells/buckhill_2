9dcf1289ba5cc49c5dd31deaab4e3c4d319aad4a
Lisa Wells <lisawells999@gmail.com>
2016-07-22 11:48:00 -0400

    abstract contact template images to custom fields

--- page-contact.php
+++ page-contact.php
@@ -6,14 +6,23 @@
 //* Template Name: Contact
 
 
+        // <!-- example from formandfunction three-top, with echo -->
+        // <!-- <img src="'. get_home_url() . genesis_get_custom_field( 'Image_1_url' ) .'" alt="'. genesis_get_custom_field( 'Image_1_alt' ) .'"> -->
+        // <!-- usage in dashboard:
+        // Image_1_url -> /wp-content/uploads/2016/06/master-bedroom-statement-wall-grasscloth-wallpaper-linen-drum-shade-pendant.jpg
+        // Image_1_alt -> Master Bedroom Statement Wall Grasscloth Wallpaper Linen Drum Shade Pendant
+        // -->
+
 add_action('genesis_after_content_sidebar_wrap', 'bh_contact_add_imageband');
-function bh_contact_add_imageband() { ?>
+function bh_contact_add_imageband() {
+    echo '
     <div class="contact-imageband">
-        <img class="size-full wp-image-287 contact-imageband-img" src="http://staging3.buckhillbrewery.com/wp-content/uploads/2016/06/horse-parking.jpg" alt="Horse Parking" width="500" height="324" />
-        <img class="size-full wp-image-288 contact-imageband-img" src="http://staging3.buckhillbrewery.com/wp-content/uploads/2016/06/Buck-Hill-Brewery-Drections.jpg" alt="Buck-Hill-Brewery-Drections" width="500" height="324" />
-        <img class="size-full wp-image-289 contact-imageband-img" src="http://staging3.buckhillbrewery.com/wp-content/uploads/2016/06/restaurant-motorcycle-parking.jpg" alt="Motorcycle Parking" width="500" height="324" />
-    </div>
-<?php }
+        <img src="'. get_home_url() . genesis_get_custom_field( 'Image_1_url' ) .'" alt="'. genesis_get_custom_field( 'Image_1_alt' ) .'"
+        <img src="'. get_home_url() . genesis_get_custom_field( 'Image_2_url' ) .'" alt="'. genesis_get_custom_field( 'Image_2_alt' ) .'"
+        <img src="'. get_home_url() . genesis_get_custom_field( 'Image_3_url' ) .'" alt="'. genesis_get_custom_field( 'Image_3_alt' ) .'"
+
+    </div>';
+}
 
 //* Run the Genesis loop
 genesis();
