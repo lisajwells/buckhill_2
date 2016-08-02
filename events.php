9a67d6124806300482045f8d46084c8a8acf4840
Lisa Wells <lisawells999@gmail.com>
2016-08-02 11:03:45 -0400

    add events template

---
+++ events.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Genesis Sample.
+ */
+
+//* Template Name: Events
+
+add_action('genesis_after_content_sidebar_wrap', 'bh_contact_add_imageband');
+function bh_contact_add_imageband() {
+    echo '<div class="contact-imageband">';
+        echo '<img class="size-full contact-imageband-img" src="'. get_home_url() . genesis_get_custom_field( 'Image_1_url' ) .'" alt="'. genesis_get_custom_field( 'Image_1_alt' ) .'">';
+        echo '<img class="size-full contact-imageband-img" src="'. get_home_url() . genesis_get_custom_field( 'Image_2_url' ) .'" alt="'. genesis_get_custom_field( 'Image_2_alt' ) .'">';
+        echo '<img class="size-full contact-imageband-img" src="'. get_home_url() . genesis_get_custom_field( 'Image_3_url' ) .'" alt="'. genesis_get_custom_field( 'Image_3_alt' ) .'">';
+
+    echo '</div>';
+}
+
+//* Run the Genesis loop
+genesis();
--- functions.php
+++ functions.php
@@ -153,7 +153,7 @@ function genesis_sample_comments_gravatar( $args ) {
 add_filter( 'genesis_pre_get_option_site_layout', 'bh_event_do_layout' );
 function bh_event_do_layout( $opt ) {
     // if( tribe_is_event() && is_single() ) { // tribe_is_event() isn't working. the page layout doesn't change with this
-    if( is_single() ) { // Modify the conditions to apply the layout to here
+    if( is_single() ) {
         $opt = 'content-sidebar';
         return $opt;
     }
