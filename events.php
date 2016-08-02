c1f3c11a8dfb4deef41946c7868d2135a3a0fce5
Lisa Wells <lisawells999@gmail.com>
2016-08-02 11:40:04 -0400

    events: move buttons to entry footer

--- events.php
+++ events.php
@@ -5,13 +5,19 @@
 
 //* Template Name: Events
 
+
+add_action('genesis_entry_footer', 'bh_events_add_buttons');
+function bh_events_add_buttons() {
+
+    echo '<a class="button" href="#">Book a Private Event</a><a class="button" href="#">Directions</a>';
+}
+
 add_action('genesis_after_content_sidebar_wrap', 'bh_contact_add_imageband');
 function bh_contact_add_imageband() {
     echo '<div class="contact-imageband">';
         echo '<img class="size-full contact-imageband-img" src="http://staging3.buckhillbrewery.com/wp-content/uploads/2016/07/Full-Bar-and-Specialty-Cocktails.jpg" alt="Full Bar and Specialty Cocktails">';
         echo '<img class="size-full contact-imageband-img" src="http://staging3.buckhillbrewery.com/wp-content/uploads/2016/07/Race-Farms.png" alt="Race Farms">';
         echo '<img class="size-full contact-imageband-img" src="http://staging3.buckhillbrewery.com/wp-content/uploads/2016/07/Tables-Made-From-Local-Wood.jpg" alt="Tables-Made-From-Local-Wood">';
-
     echo '</div>';
 }
 
