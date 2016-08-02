e30f94cd910c381e1b0d973ee214445006c2f8e8
Lisa Wells <lisawells999@gmail.com>
2016-08-02 11:08:57 -0400

    hard code dummy images in events template

--- events.php
+++ events.php
@@ -8,9 +8,9 @@
 add_action('genesis_after_content_sidebar_wrap', 'bh_contact_add_imageband');
 function bh_contact_add_imageband() {
     echo '<div class="contact-imageband">';
-        echo '<img class="size-full contact-imageband-img" src="'. get_home_url() . genesis_get_custom_field( 'Image_1_url' ) .'" alt="'. genesis_get_custom_field( 'Image_1_alt' ) .'">';
-        echo '<img class="size-full contact-imageband-img" src="'. get_home_url() . genesis_get_custom_field( 'Image_2_url' ) .'" alt="'. genesis_get_custom_field( 'Image_2_alt' ) .'">';
-        echo '<img class="size-full contact-imageband-img" src="'. get_home_url() . genesis_get_custom_field( 'Image_3_url' ) .'" alt="'. genesis_get_custom_field( 'Image_3_alt' ) .'">';
+        echo '<img class="size-full contact-imageband-img" src="http://staging3.buckhillbrewery.com/wp-content/uploads/2016/07/Full-Bar-and-Specialty-Cocktails.jpg" alt="Full Bar and Specialty Cocktails">';
+        echo '<img class="size-full contact-imageband-img" src="http://staging3.buckhillbrewery.com/wp-content/uploads/2016/07/Race-Farms.png" alt="Race Farms">';
+        echo '<img class="size-full contact-imageband-img" src="http://staging3.buckhillbrewery.com/wp-content/uploads/2016/07/Tables-Made-From-Local-Wood.jpg" alt="Tables-Made-From-Local-Wood">';
 
     echo '</div>';
 }
